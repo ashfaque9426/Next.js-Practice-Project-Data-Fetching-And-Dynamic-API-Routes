da81d60d322f943f18552ea368d4e57dbd5c3e80
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-09-20 21:26:53 +0600

    some styles fixed for single user page

--- src/app/showSingleUser/[_id]/layout.jsx
+++ src/app/showSingleUser/[_id]/layout.jsx
@@ -4,16 +4,18 @@ import Image from 'next/image'
 const layout = ({ children }) => {
     return (
         <main className='md:h-[60vh] flex flex-col md:flex-row justify-center items-center gap-12' role='main'>
-            <Image
-                className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]  aspect-ratio"
-                src="/testimonial-image-4.jpg"
-                alt="Next.js Logo"
-                width={315}
-                height={0}
-                loading='lazy'
-                placeholder='blur'
-                blurDataURL='/blur-1889747_640.jpg'
-            />
+            <figure className='w-full md:w-1/2 flex justify-center md:justify-end'>
+                <Image
+                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]  aspect-ratio"
+                    src="/testimonial-image-4.jpg"
+                    alt="Next.js Logo"
+                    width={315}
+                    height={0}
+                    loading='lazy'
+                    placeholder='blur'
+                    blurDataURL='/blur-1889747_640.jpg'
+                />
+            </figure>
             {children}
         </main>
     );
--- src/app/showSingleUser/[_id]/page.jsx
+++ src/app/showSingleUser/[_id]/page.jsx
@@ -14,8 +14,8 @@ const page = ({params}) => {
     }, []);
 
     return (
-        user === null ? <h1 className='text-2xl font-bold text-center'>Loading</h1> :
-        <article className='flex flex-col justify-center items-start gap-5'>
+        user === null ? <h1 className='text-2xl font-bold md:w-1/2'>Loading</h1> :
+        <article className='w-full p-12 md:w-1/2 md:p-0 flex flex-col justify-center items-start gap-5'>
             <h1 className='text-2xl font-bold'>Hi {user?.userName}</h1>
             <p className='text-xl font-semibold'>Your Email: {user?.email}</p>
             <p className='text-lg font-semibold'>Your Password: {user?.password}</p>
